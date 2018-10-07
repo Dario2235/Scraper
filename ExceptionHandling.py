072ef0bc4012a4c94149bb5b44c4820d87248d76
Dario2235 <36301846+Dario2235@users.noreply.github.com>
2018-10-07 15:36:48 +0200

    Update ExceptionHandling.py

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -2,7 +2,7 @@
 #
 #
 #
-# Author: Dario Weeink
+# Author: Dario
 #
 
 
@@ -13,42 +13,3 @@ class ExceptionTemplate(Exception):
 class WrongStatusCode(AttributeError):
     def __init__(self):
         Exception.__init__(self, "Site is not readable, No status code 200")
-
-
-class AttrErrorIP(AttributeError):
-    def __init__(self):
-        Exception.__init__(self, "Attribute error catched, probably IPv6 packet.")
-
-
-class GenError(Exception):
-    def __init__(self):
-        Exception.__init__(self, "Unknown error catched, probably a timeout error.")
-
-
-class NoFileSelected(AttributeError):
-    def __init__(self):
-        Exception.__init__(self, "No image selected")
-
-
-class WrongImageExtension(Exception):
-    def __init__(self):
-        Exception.__init__(self, "No 001 format image selected")
-
-
-class FileDoesNotExist(Exception):
-    def __init__(self):
-        Exception.__init__(self, "File does not exist")
-
-
-class WrongOptionNumber(Exception):
-    def __init__(self):
-        Exception.__init__(self, "Choose a correct option number")
-
-
-class FileHasNoMeta(Exception):
-    def __init__(self, filename):
-        Exception.__init__(self, "File " + filename + " has no meta information and is not added to the list")
-
-class FileOpenMail(IOError):
-    def __init__(self):
-        IOError.__init__(self, "Please close mail.csv before continuing.")
