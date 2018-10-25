2928e0765de45712e612428e1226b68f7abf71c3
Dario2235 <36301846+Dario2235@users.noreply.github.com>
2018-10-25 14:36:56 +0200

    Update Scp.py

--- Scp.py
+++ Scp.py
@@ -18,10 +18,10 @@ class Scp:
     port = 22
 
     # De user moet hier zelf nog ingevuld worden hardcoded is het makkelijkste
-    user = "ipfit6"
+    user = ""
 
     # Hier moet zelf het password nog ingevuld worden
-    password = "Groep2"
+    password = ""
 
 
     def createSSHClient(self):
